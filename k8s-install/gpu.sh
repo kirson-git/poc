c9af1c133b6646bdb0dbc9bc9f405bffc4f4d3c7
Erez Kirson <kirson@run.ai>
2022-07-17 12:15:42 +0300

    added gpu sh file

---
+++ k8s-install/gpu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+helm repo add nvidia https://helm.ngc.nvidia.com/nvidia \
+&& helm repo update
+
+helm install --wait --generate-name \
+     -n gpu-operator --create-namespace \
+     nvidia/gpu-operator
