11d4d094cf58968cdc6f73ad197705c107eb8ade
Erez Kirson <kirson@run.ai>
2023-07-24 11:59:28 +0300

    Cleaning Folder Structure

--- infra/kubespray/create-registry.sh
+++
@@ -1,8 +0,0 @@
-DOMAIN=your-fqdn
-CERT=path-to-crt
-KEY=path-to-key
-USER=use-name
-PASSWD=password 
-### Create Storage Path
-mkdir /quay-storage/
-/mirror-registry install --sslCert $CERT --sslkey $KEY   --targetHostname $DOMAIN  --initUser=$USER --initPassword=$PASSWD  --pgStorage /quay-storage/ --quayStorage /quay-storage/ --quayRoot /quay-storage/ -v
