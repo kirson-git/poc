1421eb08e985fb9148c34f7651ed807cb91e3745
Erez Kirson <kirson@run.ai>
2024-03-08 01:30:12 +0200

    update

--- air-gapped/openshift/replace-ingress.sh
+++ air-gapped/openshift/replace-ingress.sh
@@ -1,28 +1,7 @@
 
-ROOT=
-KEY=
-CERT=
-
-kubectl patch proxy/cluster      --type=merge      --patch='{"spec":{"trustedCA":{"name":"custom-ca"}}}'
 
 
-oc create configmap custom-ca \
-     --from-file=ca-bundle.crt=./rootCA.pem \
-     -n openshift-config
-Step 2
-oc patch proxy/cluster \
-( Wait ! check with oc get co )
-
-kubectl patch proxy/cluster      --type=merge      --patch='{"spec":{"trustedCA":{"name":"custom-ca"}}}'
-
-
-Step 3
-oc create secret tls wild-runai \
-     --cert=/alex/runai.crt \
-     --key=/alex/runai.key \
-     -n openshift-ingress
-Step 4
-oc patch ingresscontroller.operator default \
-     --type=merge -p \
-     '{"spec”:{"defaultCertificate": {"name": "wild-runa"}}}' \
-     -n openshift-ingress-operator
+oc create configmap custom-ca  --from-file=ca-bundle.crt=./rootCA.pem   -n openshift-config
+oc patch proxy/cluster      --type=merge      --patch='{"spec":{"trustedCA":{"name":"custom-ca"}}}'
+oc create secret tls wild-runai --cert=runai.crt  --key=runai.key  -n openshift-ingress
+oc patch ingresscontroller.operator default      --type=merge -p  '{"spec”:{"defaultCertificate": {"name": "wild-runai"}}}'   -n openshift-ingress-operator
