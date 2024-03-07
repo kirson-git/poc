14696b758b71f78a052d90d5734813cbdd715ac1
Erez Kirson <kirson@run.ai>
2024-03-08 01:59:30 +0200

    added Kubespray v2.21.0

--- kubernetes/installation-tools/kubespray-2.21.0/contrib/terraform/openstack/modules/compute/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    openstack = {
-      source = "terraform-provider-openstack/openstack"
-    }
-  }
-  experiments = [module_variable_optional_attrs]
-  required_version = ">= 0.14.0"
-}
